1215524a15a06d71301efe46a5f0e8d649c3bf32
Brenna Kjorness <brennakj3@gmail.com>
2023-03-05 20:55:03 -0800

    Added everything

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Feed from './components/Feed';
+import HomePage from './components/HomePage';
+import CreatePost from './components/CreatePost';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+//    <img src="" alt="React Image" />
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>Bruin Thrift</h1>
-      </header>
-    </div>
+    <Router>
+      <div id="navigation">
+          <div>
+           <Link id = "links" to="/">
+           <h1 id="navname">Bruin Thrift</h1>
+           </Link>
+            <img id="logo" src="https://altusimpact.com/wp-content/uploads/2020/05/white-leaf.png"/>
+          </div>
+          <div>
+            <Link id = "links" to="/">Home</Link>{"    |    "}
+            <Link id = "links" to="/feed">Feed</Link>{"    |    "}
+            <Link id="links" to ="/writepost">Create a Post</Link>
+          </div>
+      </div>
+      <div id="content">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/feed" element={<Feed/>}/>
+          <Route path="/writepost" element={<CreatePost/>}/>
+        </Routes>
+        
+      </div>
+    </Router>
+
   );
 }
 
